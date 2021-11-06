58bcd30164b19ec8e70e5b65bd5ba4755c410205
adrewrog <adrewrog@adrewrog-desktop.localdomain>
2021-11-06 15:08:05 -0500

    Standard template with mux router and /ping, /sping

--- src/service.go
+++ src/service.go
@@ -1,47 +1,23 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
 	"github.com/gorilla/mux"
-	"io"
 	"net/http"
 )
 
-type result struct {
-	Data string
-}
-
-func getHome(w http.ResponseWriter, r *http.Request) {
+func ping(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("Hello World! GoLang is pretty fun so far!"))
+	w.Write([]byte("healthy"))
 }
 
-func getPing(w http.ResponseWriter, r *http.Request) {
-	var param string = mux.Vars(r)["key"]
-
-	if param == "ping" {
-
-		data := result{ // Create an instance of result struct
-			Data: "pong",
-		}
-		resp, err := json.Marshal(data)
-		if err != nil {
-			fmt.Println("An error occurred marshaling json w/ key " + param)
-		} else { // Return the new marshaled json object
-			w.WriteHeader(http.StatusOK)
-			w.Header().Set("Content-Type", "application/json")
-			io.WriteString(w, string(resp))
-		}
-
-	} else {
-		w.WriteHeader(http.StatusOK) // Still want to set status 200 s
-	}
+func sping(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("healthy"))
 }
 
 func main() {
-	router := mux.NewRouter() // Create gorilla mux router
-	router.HandleFunc("/", getHome).Methods("GET")
-	router.HandleFunc("/ping/{key}", getPing).Methods("GET") // Pass key into test endpoint
+	router := mux.NewRouter() // Uses mux router
+	router.HandleFunc("/ping", ping).Methods(http.MethodGet)
+	router.HandleFunc("/sping", sping).Methods(http.MethodGet)
 	http.ListenAndServe(":8080", router)
 }
