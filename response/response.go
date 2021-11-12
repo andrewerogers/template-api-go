0ea5382e49d7608a5e4082fad6578ebd387a0d2c
adrewrog <adrewrog@adrewrog-desktop.localdomain>
2021-11-11 19:49:22 -0600

    Created Error Response entity

--- response/response.go
+++ response/response.go
@@ -14,3 +14,14 @@ func (gr GeneralResponse) Write(w http.ResponseWriter) {
 	w.WriteHeader(http.StatusOK)
 	w.Write(json)
 }
+
+type ErrorResponse struct {
+	Status int `json:"status"`
+	Message string `json:"message"`
+}
+
+func (er ErrorResponse) Write(w http.ResponseWriter) {
+	json, _ := json.Marshal(er)
+	w.WriteHeader(er.Status)
+	w.Write(json)
+}
